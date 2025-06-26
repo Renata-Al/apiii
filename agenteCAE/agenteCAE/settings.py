44ea105dd4137ee6bd81beb431b3b39a6000b7d6
Renata-Al <souz4renata2007@gmail.com>
2025-06-25 20:22:14 -0400

    manage

--- agenteCAE/agenteCAE/settings.py
+++ agenteCAE/agenteCAE/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'agenteCAE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'renata',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
